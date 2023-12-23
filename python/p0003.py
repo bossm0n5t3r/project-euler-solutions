6d48af2a3d2fb65fc69601b5ae1526d5dd8aade2
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-12-24 05:01:09 +0900

    Largest Prime Factor

--- python/p0003.py
+++ python/p0003.py
@@ -1,5 +1,6 @@
 #  Solution to Project Euler Problem 3
 
+
 def sol():
     n = 600851475143
     i = 2
@@ -8,6 +9,7 @@ def sol():
             n //= i
         i += 1
     return str(n)
+
 
 if __name__ == "__main__":
     print(sol())
