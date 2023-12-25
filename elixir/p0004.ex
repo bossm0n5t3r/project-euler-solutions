9e8ff34127d073baf6f26834b26e722c72501d2f
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-12-25 23:00:00 +0900

    Largest Palindrome Product

---
+++ elixir/p0004.ex
@@ -0,0 +1,23 @@
+defmodule P0004 do
+  @moduledoc """
+  Largest Palindrome Product
+  Problem 4
+
+  https://projecteuler.net/problem=4
+  """
+
+  def solve() do
+    Enum.to_list(100..999)
+    |> Enum.map(fn x -> Enum.to_list(100..999) |> Enum.map(fn y -> x * y end) end)
+    |> List.flatten()
+    |> Enum.filter(&is_palindrome/1)
+    |> Enum.max()
+  end
+
+  def is_palindrome(num) do
+    Integer.to_string(num) == Integer.to_string(num) |> String.reverse()
+  end
+end
+
+P0004.solve()
+|> IO.puts()
