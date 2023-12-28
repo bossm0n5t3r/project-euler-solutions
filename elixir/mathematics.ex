5e1ceecd14b27c79dc6f3c2438e9e2328522b09c
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-12-29 05:37:30 +0900

    feat: Edit Mathematics
    - Add is_prime? method

--- elixir/mathematics.ex
+++ elixir/mathematics.ex
@@ -3,4 +3,12 @@ defmodule Mathematics do
   def gcd(a, 0), do: a
 
   def lcm(a, b), do: div(a * b, gcd(a, b))
+
+  def is_prime?(num) do
+    if num == 2 or num == 3 do
+      true
+    else
+      Enum.all?(2..trunc(:math.sqrt(num)), fn x -> rem(num, x) != 0 end)
+    end
+  end
 end
