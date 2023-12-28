e6caaa27c16867079291f8c7cf65018fa3fbbf54
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-12-29 00:53:10 +0900

    Largest Product in a Series

--- elixir/p0008.ex
+++ elixir/p0008.ex
@@ -10,15 +10,24 @@ defmodule P0008 do
 
   def solve() do
     FileUtil.read_file("p0008.txt")
-    |> string_to_integer_list
+    |> string_to_integer_list_using_codepoints
+    # |> string_to_integer_list_using_charlist # or use this
     |> Enum.chunk_every(13, 1, :discard)
     |> Enum.map(fn x -> Enum.reduce(x, 1, fn y, acc -> y * acc end) end)
     |> Enum.max()
   end
 
-  def string_to_integer_list(string) do
+  # https://hexdocs.pm/elixir/1.16.0/String.html#codepoints/1
+  def string_to_integer_list_using_codepoints(string) do
     string
-    |> to_charlist
+    |> String.codepoints()
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  # https://hexdocs.pm/elixir/1.16.0/String.html#to_charlist/1
+  def string_to_integer_list_using_charlist(string) do
+    string
+    |> to_charlist()
     |> Enum.map(&(&1 - ?0))
   end
 end
