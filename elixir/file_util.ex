25d4aa58fe245ec37c6be4687c2f2ce3d6c0f156
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-12-30 02:01:10 +0900

    fix: Edit FileUtil
    - Add read_lines

--- elixir/file_util.ex
+++ elixir/file_util.ex
@@ -12,4 +12,18 @@ defmodule FileUtil do
         System.halt(1)
     end
   end
+
+  @spec read_lines(String.t()) :: list(String.t())
+  def read_lines(filename) do
+    path = Path.expand("../resources/#{filename}", __DIR__)
+
+    case File.read(path) do
+      {:ok, lines} ->
+        lines |> String.split("\n", trim: true)
+
+      {:error, _} ->
+        IO.puts("Error: #{filename} file not found")
+        System.halt(1)
+    end
+  end
 end
