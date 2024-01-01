d7bc39feb2aa00085b73764a4acc326724b28383
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-01-02 00:27:21 +0900

    Even Fibonacci Numbers

--- elixir/p0002.ex
+++ elixir/p0002.ex
@@ -1,3 +1,5 @@
+Code.require_file("./mathematics.ex", __DIR__)
+
 defmodule P0002 do
   @moduledoc """
   Even Fibonacci Numbers
@@ -7,16 +9,10 @@ defmodule P0002 do
   """
 
   def solve() do
-    fib()
+    Mathematics.fib()
     |> Enum.take_while(fn x -> x < 4_000_000 end)
     |> Enum.filter(fn x -> rem(x, 2) == 0 end)
     |> Enum.sum()
-  end
-
-  def fib() do
-    Stream.unfold({0, 1}, fn {current, next} ->
-      {current, {next, current + next}}
-    end)
   end
 end
 
