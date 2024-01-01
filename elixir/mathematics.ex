08484ececa84f8027ed9577aaa4c438fd765b713
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-01-01 23:05:31 +0900

    fix: Edit Mathematics
    - Add get_sum_of_proper_divisors method

--- elixir/mathematics.ex
+++ elixir/mathematics.ex
@@ -19,4 +19,12 @@ defmodule Mathematics do
   def factorial(1), do: 1
 
   def combination(n, r), do: div(factorial(n), factorial(r) * factorial(n - r))
+
+  def get_sum_of_proper_divisors(num) when num == 1, do: 0
+
+  def get_sum_of_proper_divisors(num) do
+    1..(num - 1)
+    |> Enum.filter(fn x -> rem(num, x) == 0 end)
+    |> Enum.sum()
+  end
 end
